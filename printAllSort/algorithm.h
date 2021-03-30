76f3b5bacda25dcf5adbfdae9e490753b62cb1b7
0A-liang <59197310+0A-liang@users.noreply.github.com>
2021-03-30 10:51:19 +0800

    Create algorithm.h

---
+++ printAllSort/algorithm.h
@@ -0,0 +1,13 @@
+#ifndef ALGORITHM_H
+#define ALGORITHM_H
+
+#include <vector>
+using namespace std;
+
+void insertion_sort(vector<int>&);
+void selection_sort(vector<int>&);
+void bubble_sort(vector<int>&);
+void merge_sort(vector<int>&, int, int);
+void heap_sort(vector<int>&);
+void merge_called(vector<int> & A);
+#endif
