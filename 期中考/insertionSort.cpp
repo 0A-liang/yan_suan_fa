7f1ea7de32d8537800dd91abfb4454e7fead8198
0A-liang <59197310+0A-liang@users.noreply.github.com>
2021-04-28 00:21:10 +0800

    Update insertionSort.cpp

--- 期中考/insertionSort.cpp
+++ 期中考/insertionSort.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-using iter = vector<int>::iterator;
 void InsertionSort(vector<int>& A)
 {
-	for (iter it = A.begin() + 1; it != A.end(); it++)
+	for (int i=1;i<A.size();i++)
 	{
-		int key = *it;
-		iter it2 = it - 1;
-		while (it2>=A.begin()&&key<*it2)
+		int key = A[i];
+		int j = i - 1;
+		while (j>=0&&key<A[j])
 		{
-			*(it2 + 1) = *it2;
-			it2--;
+			A[j+1] = A[j];
+			j--;
 		}
-		*(it2 + 1) = key;
-		for (iter itr = A.begin(); itr != A.end(); itr++)
+		A[j+1] = key;
+		for (int k=0;k<A.size();k++)
 		{
-			cout << *itr << " ";
+			cout << A[k] << " ";
 		}
 		cout << endl;
 	}
